8b32f9e1636e83174a54981780a37c32a790df4d
ucozlab <artem@ucoz-team.net>
2014-12-17 19:13:40 +0200

    zalil vse

---
+++ main.js
@@ -0,0 +1,31 @@
+﻿//menuha
+
+        $("header nav").click(function() {
+            if($(window).width() <= 1024) {
+               $("header nav ul").removeClass("tableimp");
+                if ($('header nav ul').hasClass('active')) {
+                    $("header nav ul").fadeOut("700");
+                    $("header nav ul").removeClass("active");
+                    $(this).removeClass("nav_back");
+                }
+                else{
+                    $("header nav ul").fadeIn("700");
+                    $("header nav ul").addClass("active");
+                    $(this).addClass("nav_back");
+                }
+            }
+            else{
+                $("header nav ul").removeClass("nav_back");
+                $("header nav ul").addClass("tableimp");
+            }
+        });
+
+        $( window ).resize(function() {
+		if($(window).width() > 1023) {
+		    $("header nav").removeClass("nav_back");
+		    $("header nav ul").addClass("tableimp");
+		}
+		else{
+		    $("header nav ul").removeClass("tableimp");
+		}
+	}); 
