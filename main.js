3bb31fb0991c279ee17e5f2adb3eb9cb97db3896
ucozlab <artem@ucoz-team.net>
2014-12-18 20:48:52 +0200

    modify 2

--- main.js
+++ main.js
@@ -5,14 +5,18 @@ function add(){
         sheduler.textContent = "";
     }
     var text = document.querySelector('.send').value;
-    var task = '<div class="table-row"><div class="table-cell">' + globalcount + '</div><div class="table-cell">' + text + '</div></div><div>';
+    var task = '<div class="table-row"><div class="table-cell">' + globalcount + '</div><div class="table-cell">' + text + '</div><div class="table-cell ok"></div><div class="table-cell delete"></div></div>';
     sheduler.innerHTML += task;
     globalcount++;
+    document.querySelector(".ok").addEventListener('click',function(event){
+        event.target.classList.remove('ok');
+        event.target.classList.add('ready');
+        event.target.parentNode.classList.add('activated');
+    });
 }
 document.querySelector('.button').addEventListener('click',function(){
     add();
 });
-
 
 document.querySelector('input[type=text]').onkeypress = function(event){
     if(event.keyCode == 13){
