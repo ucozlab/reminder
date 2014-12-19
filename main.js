c1a131fe803474299ac3e7068a6f0802a615682f
ucozlab <artem@ucoz-team.net>
2014-12-19 20:10:45 +0200

    last var

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 ﻿globalcount = 1;
+globalcount2 = 1;
 function add(){
     var sheduler = document.querySelector('.sheduler .table');
-    if(sheduler.textContent == "empty"){
-        sheduler.textContent = "";
+    var check = document.querySelector(".empty");
+    if (check.style.display != "none"){
+        check.style.display = "none";
     }
     var text = document.querySelector('.send').value;
-    var task = '<div class="table-row"><div class="table-cell">' + globalcount + '</div><div class="table-cell">' + text + '</div><div class="table-cell ok"></div><div class="table-cell delete"></div></div>';
+    var task = '<div class="table-row"><div class="table-cell">' + globalcount + '</div><div class="table-cell">' + text + '</div><div class="table-cell cell ok"></div><div class="table-cell delete"></div></div>';
     sheduler.innerHTML += task;
     globalcount++;
-    document.querySelector(".ok").addEventListener('click',function(event){
-        event.target.classList.remove('ok');
-        event.target.classList.add('ready');
-        event.target.parentNode.classList.add('activated');
-    });
+    globalcount2++;
+    addclick();
 }
 document.querySelector('.button').addEventListener('click',function(){
     add();
@@ -23,3 +22,42 @@ document.querySelector('input[type=text]').onkeypress = function(event){
         add();
     }
 };
+function addclick(){ //add click functions on every new row;
+
+    [].filter.call(document.querySelectorAll(".cell"), function (el) {
+        return this;
+    }).forEach(function (el) {
+        el.addEventListener('click',function(event){
+
+            function hasClass(element, cls) {
+                return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+            }
+            if(hasClass(event.target, 'ready')){
+                event.target.classList.remove('ready');
+                event.target.classList.add('ok');
+                event.target.parentNode.classList.remove('activated');
+            }
+            else{
+                event.target.classList.remove('ok');
+                event.target.classList.add('ready');
+                event.target.parentNode.classList.add('activated');
+            }
+        });
+
+    });
+    [].filter.call(document.querySelectorAll(".delete"), function (el2) {
+        return this;
+    }).forEach(function (el2) {
+        el2.addEventListener('click',function(event){
+            event.target.parentNode.parentNode.removeChild(this.parentNode);
+            globalcount2--;
+            if(globalcount2 == 1){
+                document.querySelector(".empty").style.display = "block";
+            }
+        });
+
+    });
+
+}
+
+//add click on ok button
